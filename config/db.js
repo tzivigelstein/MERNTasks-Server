efd8cea4f4707c5bd23058e448624202d32637f9
Tzivi <stavgamer00@gmail.com>
2020-09-15 15:51:52 -0300

    Commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'variables.env' })
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO, {
+        await mongoose.connect(process.env.TEST, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
