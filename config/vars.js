7a3e00d20d175320910b8174051e10936af6c89a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-07-14 10:38:46 -0300

    Test backend in production

--- config/vars.js
+++ config/vars.js
@@ -13,7 +13,7 @@ const enviroment = NODE_ENV || 'development'
 const port = PORT || 4000
 const frontendUrl = enviroment === 'production' ? PRODUCTION_FRONTEND_URL : DEVELOPMENT_FRONTEND_URL
 
-console.log('Node enviroment', NODE_ENV)
+console.log('Node enviroment', NODE_ENV, 'Enviroment', enviroment)
 
 module.exports = {
   database,
