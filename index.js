95357b603fa446ce8d75479b1618a4ee1799d2e4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-08-02 17:01:01 -0300

    Delete ip message

--- index.js
+++ index.js
@@ -2,24 +2,6 @@ const express = require('express')
 const connectDB = require('./config/db')
 const cors = require('cors')
 const { port, frontendUrl } = require('./config/vars')
-const { networkInterfaces } = require('os')
-
-const nets = networkInterfaces()
-const results = {}
-
-for (const name of Object.keys(nets)) {
-  for (const net of nets[name]) {
-    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-    if (net.family === 'IPv4' && !net.internal) {
-      if (!results[name]) {
-        results[name] = []
-      }
-      results[name].push(net.address)
-    }
-  }
-}
-
-const localIp = results['Wi-Fi'][0]
 
 //Crear server
 const app = express()
@@ -41,5 +23,5 @@ app.use('/api/tasks', require('./routes/tasks'))
 
 //Init server
 app.listen(port, () => {
-  console.log(`Server deployed in http://${localIp}:${port}`)
+  console.log(`Server deployed in ${port}`)
 })
